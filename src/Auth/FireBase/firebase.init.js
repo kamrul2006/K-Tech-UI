b26d5fe9a31d4f9bf877e9dcf30cc30f9104618f
Kamrul Islam Apurba <kamrulislamapurba@gmail.com>
2025-01-24 21:27:29 +0600

    Firebase setup done

--- src/Auth/FireBase/firebase.init.js
+++ src/Auth/FireBase/firebase.init.js
@@ -1,10 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
@@ -17,5 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
